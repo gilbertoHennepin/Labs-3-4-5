a10f51a47a2b9197709f1a910a449736877f1d8b
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-24 16:39:52 -0500

    Create MilesToKM.java

---
+++ Lab3/MilesToKM.java
@@ -0,0 +1,19 @@
+package week3;
+
+import static input.InputUtils.doubleInput;
+
+public class MilesToKM {
+    public static void main(String[] args) {
+        double miles = doubleInput("Enter a number of miles");
+        double km = milesToKM(miles);
+        System.out.println(miles + " miles is equal to " + km + "kilometers");
+
+    }
+
+    private static double milesToKM(double miles) {
+        return miles * 1.6;
+
+//        double km = miles * 1.6;
+//        return km:
+    }
+}
