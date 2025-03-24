16ff3bdb2ce434cb4e69b8ad1826bbf3ab578f39
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-24 16:42:28 -0500

    Create ArrayLoop.java

---
+++ Lab4/ArrayLoop.java
@@ -0,0 +1,11 @@
+package week4;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+
+        String[] days = {"mon", "tue", "wed", "thu", "fri"};
+        for (String day : days) {
+            System.out.println(day);
+        }
+    }
+}
