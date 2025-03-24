f44107755a9c71bb9c8cce621b3331a08c73c64a
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-03-24 16:39:20 -0500

    Create Marathon.java

---
+++ Lab3/Marathon.java
@@ -0,0 +1,23 @@
+package week3;
+
+public class Marathon {
+    public static void main(String[] args) {
+
+        double currentDistance = 1;
+        double targetDistance = 26.2;
+        double percentIncrease = 1.10; // increase by 10%
+
+        int week = 1;
+//          loop until current distance is greater than the target
+//        the loop will count the number of weeks
+        while (currentDistance < targetDistance) {
+            System.out.println(week + " " + currentDistance);
+            currentDistance = currentDistance * percentIncrease; // increase by 10%
+            week++; // count number of weeks
+        }
+//      need to print one more line - the distance that is greater
+//        than the marathon distance
+        System.out.println(week + " " + currentDistance);
+
+    }
+}
